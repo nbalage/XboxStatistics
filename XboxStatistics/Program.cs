2fc798124a7179161f4f551b2a3ac7095da16c6d
Balazs Nemeth <Balazs_Nemeth@epam.com>
2019-05-02 13:38:17 +0200

    2

--- XboxStatistics/Program.cs
+++ XboxStatistics/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using XboxStatistics.Models;
 
 namespace XboxStatistics
 {
@@ -12,13 +14,13 @@ namespace XboxStatistics
             Question("How many games do I have?", HowManyGamesDoIHave);
             Question("How many games have I completed?", HowManyGamesHaveICompleted);
             Question("How much Gamerscore do I have?", HowMuchGamescoreDoIHave);
-            //Question("How many days did I play?", HowManyDaysDidIPlay);
+            Question("How many days did I play?", HowManyDaysDidIPlay);
             //Question("Which game have I spent the most hours playing?", WhichGameHaveISpentTheMostHoursPlaying);
             Question("In which game did I unlock my latest achievement?", InWhichGameDidIUnlockMyLatestAchievement);
             Question("List all of my statistics in Binding of Isaac:", ListAllOfMyStatisticsInBindingOfIsaac);
             Question("How many achievements did I earn per year?", HowManyAchievementsDidIEarnPerYear);
-            //Question("List all of my games where I have earned a rare achievement", ListAllOfMyGamesWhereIHaveEarnedARareAchievement);
-            //Question("List the top 3 games where I have earned the most rare achievements", ListTheTop3GamesWhereIHaveEarnedTheMostRareAchievements);
+            Question("List all of my games where I have earned a rare achievement", ListAllOfMyGamesWhereIHaveEarnedARareAchievement);
+            Question("List the top 3 games where I have earned the most rare achievements", ListTheTop3GamesWhereIHaveEarnedTheMostRareAchievements);
             //Question("Which is my rarest achievement?", WhichIsMyRarestAchievement);
 
             Console.ReadLine();
@@ -45,14 +47,22 @@ namespace XboxStatistics
         static string HowManyDaysDidIPlay()
         {
             //HINT: there's a game stat property called MinutesPlayed, and as the name suggests it stored total minutes
-            //var a = Xbox.MyGames.Where(g => g.Name == "The Binding of Isaac: Rebirth"); // The Binding of Isaac: Rebirth
-            //return "";
-            throw new NotImplementedException();
+            var minutes = Xbox.GameStats.Values
+                        .SelectMany(g => g)
+                        .Where(g => g.Name == "MinutesPlayed")
+                        .Where(g => g.Value != null)
+                        .Sum(g => double.Parse(g.Value));
+            return ((int)(minutes / 60) / 24).ToString();
         }
 
         static string WhichGameHaveISpentTheMostHoursPlaying()
         {
             //HINT: there's a game stat property called MinutesPlayed, and as the name suggests it stored total minutes
+            //return Xbox.GameStats.Values
+            //            .SelectMany(g => g)
+            //            .Where(g => g.Name == "MinutesPlayed")
+            //            .Where(g => g.Value != null)
+            //            .
             throw new NotImplementedException();
         }
 
@@ -73,48 +83,51 @@ namespace XboxStatistics
                                    .Where(g => g.Name.Contains("Binding of Isaac")))
                     join s in Xbox.GameStats
                         on x.TitleId equals s.Key
-                    select new
+                    select new 
                     {
-                        xbox = x,
-                        stat = s
+                        GameTitle = x,
+                        Stat = s
                     };
-            return null;
+            return null; // isaacs.Aggregate("", (retval, stat) => retval += $"{stat.GameTitle.Name} = {stat.Stat.Value}");
         }
 
         static string HowManyAchievementsDidIEarnPerYear()
         {
             //HINT: unlocked achievements have an "Achieved" progress state
-            //var achs = Xbox.MyGames.GroupBy(g => g.LastUnlock.Year);
-            //var achs = from g in Xbox.MyGames
-            //           join a in Xbox.Achievements//.SelectMany(ac => ac.Value)
-            //               on g.TitleId equals a.Key
-            //           group g by g.LastUnlock.Year into yearGroup
-            //           orderby yearGroup.Key
-            //           select new
-            //           {
-            //               year = yearGroup.Key,
-            //               count = yearGroup.Count()
-            //           };
-            var achs = from g in (Xbox.MyGames.Where(g => g.LastUnlock != null))
-                       join a in Xbox.Achievements
-                           on g.TitleId equals a.Key
+            var achs = from a in (Xbox.Achievements.SelectMany(a => a.Value).Where(a => a.Progression.TimeUnlocked > DateTime.MinValue))
+                       group a by a.Progression.TimeUnlocked.Year into groupedAch
+                       orderby groupedAch.Key
                        select new
                        {
-                           game = g,
-                           ach = a.Value.Count()
+                           year = groupedAch.Key,
+                           count = groupedAch.Count()
                        };
-            return null;
+            return achs.Aggregate("", (retval, a) => retval += $"{a.year.ToString()}: {a.count.ToString()}\n");
         }
 
         static string ListAllOfMyGamesWhereIHaveEarnedARareAchievement()
         {
             //HINT: rare achievements have a rarity category called "Rare"
-            throw new NotImplementedException();
+            var games = Xbox.Achievements
+                            .SelectMany(a => a.Value)
+                            .Where(r => r.Rarity.CurrentCategory == "Rare")
+                            .SelectMany(a => a.TitleAssociations)
+                            .Select(a => a.Name)
+                            .Distinct();
+            return null;// games.Aggregate("", (retval, ach) => retval += $"{ach}\n");
         }
 
         static string ListTheTop3GamesWhereIHaveEarnedTheMostRareAchievements()
         {
-            throw new NotImplementedException();
+            var games = Xbox.Achievements
+                            .SelectMany(a => a.Value)
+                            .Where(r => r.Rarity.CurrentCategory == "Rare" && r.Rarity.CurrentPercentage > 0)
+                            .Select(s => new { Game = s.TitleAssociations[0].Name })
+                            .GroupBy(s => s.Game)
+                            .Select(a => new { Game = a, Count = a.Count() })
+                            .OrderByDescending(a => a.Count)
+                            .Take(3);
+            return null;
         }
 
         static string WhichIsMyRarestAchievement()
